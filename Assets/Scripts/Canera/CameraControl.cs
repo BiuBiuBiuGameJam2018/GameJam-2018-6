8aff9d56d7ae85ee6ae394e4e043049462d12ef6
WangDaxia57 <wang987662210@163.com>
2018-06-01 22:32:23 +0800

    s

--- Assets/Scripts/Canera/CameraControl.cs
+++ Assets/Scripts/Canera/CameraControl.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
-    public float nearest,furthest;
+public class CameraControl : MonoBehaviour
+{
+    public float nearest, furthest;
+    GameObject leftdown, rightup;
+    public int remainpixel = 50;
+    Vector2 p1, p2;//用来记录鼠标的位置，以便计算移动距离  
     // Use this for initialization
     float speed = 1;
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        leftdown = MainManger.Instance.Bg.transform.Find("leftdown").gameObject;
+        rightup = MainManger.Instance.Bg.transform.Find("rightup").gameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
         //Zoom out  
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
@@ -30,7 +36,7 @@ public class CameraControl : MonoBehaviour {
         }
         if (Input.mousePosition.x <= 0)
         {
-            transform.Translate(Vector3.left*Time.deltaTime*speed* Camera.main.orthographicSize);
+            transform.Translate(Vector3.left * Time.deltaTime * speed * Camera.main.orthographicSize);
         }
         if (Input.mousePosition.x >= Screen.width)
         {
