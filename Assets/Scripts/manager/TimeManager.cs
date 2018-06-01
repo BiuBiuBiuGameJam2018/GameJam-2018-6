fc7d0bfb1c2b7a57c8856677f4fa44fb9dd901ce
stst11111 <605066756@qq.com>
2018-06-01 22:01:34 +0800

    啦啦啦

--- Assets/Scripts/manager/TimeManager.cs
+++ Assets/Scripts/manager/TimeManager.cs
@@ -6,21 +6,14 @@ public enum Enum_DayNight
     Day,
     Night,
 }
-<<<<<<< HEAD
-public class TimeManager :Singleton<TimeManager> {
-    public Enum_DayNight TimeState;
-    public float DayTime;//白天持续时间
-    public float NightTime;//夜晚持续时间
-    
 
-=======
 public class TimeManager : Singleton<TimeManager> {
     public Enum_DayNight TimeState=Enum_DayNight.Day;
     public float DayTime=10f;//白天持续时间
     public float NightTime=10f;//夜晚持续时间
     public float DurationTime;///持续时间
     public float GameAllTime;///游戏总长时间
->>>>>>> e320daf1619f078aef240e9f6d8dffddac8eeb3a
+
     ///计时器
     ///
     #region 
