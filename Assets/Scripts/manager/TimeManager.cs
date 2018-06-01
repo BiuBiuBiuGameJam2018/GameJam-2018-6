e320daf1619f078aef240e9f6d8dffddac8eeb3a
WangDaxia57 <wang987662210@163.com>
2018-06-01 21:57:38 +0800

    时间管理

--- Assets/Scripts/manager/TimeManager.cs
+++ Assets/Scripts/manager/TimeManager.cs
@@ -6,12 +6,40 @@ public enum Enum_DayNight
     Day,
     Night,
 }
-public class TimeManager :Singleton<TimeManager> {
-    public Enum_DayNight TimeState;
-    public float DayTime;//白天持续时间
-    public float NightTime;//夜晚持续时间
-
-
+public class TimeManager : Singleton<TimeManager> {
+    public Enum_DayNight TimeState=Enum_DayNight.Day;
+    public float DayTime=10f;//白天持续时间
+    public float NightTime=10f;//夜晚持续时间
+    public float DurationTime;///持续时间
+    public float GameAllTime;///游戏总长时间
     ///计时器
     ///
+    #region 
+    public void Begin()
+    {
+    }
+    public void Load()
+    {
+
+    }
+
+    public void Generate()
+    {
+
+    }
+    /// <summary>
+    /// 刷新
+    /// </summary>
+    public void GameUpdate()
+    {
+
+    }
+    #endregion
+    /// <summary>
+    /// 开始计时
+    /// </summary>
+    public void StartTime(){
+
+
+        }
 }
