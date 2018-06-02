c678ff63d5d28d9cccecb5e118f992d1136601d4
stst11111 <605066756@qq.com>
2018-06-02 16:27:42 +0800

    1

--- Assets/Scripts/Canera/CameraControl.cs
+++ Assets/Scripts/Canera/CameraControl.cs
@@ -6,6 +6,8 @@ public class CameraControl : MonoBehaviour {
     public float nearest,furthest;
     GameObject leftdown, rightup;
     public int remainpixel = 50;
+    Vector2 startmousepos;
+    Vector3 startcamerapos;
     Vector2 p1, p2;//用来记录鼠标的位置，以便计算移动距离  
     // Use this for initialization
     float speed = 1;
@@ -33,29 +35,44 @@ public class CameraControl : MonoBehaviour {
             if (Camera.main.orthographicSize >=nearest)
                 Camera.main.orthographicSize -= 0.5F;
         }
-        if ((Input.mousePosition.x <= 0
-            &&Camera.main.WorldToScreenPoint(leftdown.transform.position).x< remainpixel)
-            || Camera.main.WorldToScreenPoint(rightup.transform.position).x < Screen.width- remainpixel)
+
+        if (Input.GetMouseButtonDown(2))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * speed * Camera.main.orthographicSize);
+            startmousepos = Input.mousePosition;
+            startcamerapos = transform.position;
         }
-        if ((Input.mousePosition.x >= Screen.width
-            && Camera.main.WorldToScreenPoint(rightup.transform.position).x > Screen.width- remainpixel)
-            || Camera.main.WorldToScreenPoint(leftdown.transform.position).x > remainpixel)
+        if (Input.GetMouseButton(2))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * speed * Camera.main.orthographicSize);
+
+            transform.position = startcamerapos + new Vector3(startmousepos.x - Input.mousePosition.x, startmousepos.y - Input.mousePosition.y) * 0.01f; ;
         }
-        if ((Input.mousePosition.y >= Screen.height
-            && Camera.main.WorldToScreenPoint(rightup.transform.position).y > Screen.height - remainpixel)
-            || Camera.main.WorldToScreenPoint(leftdown.transform.position).y > remainpixel)
+        else
         {
-            transform.Translate(Vector3.up * Time.deltaTime * speed * Camera.main.orthographicSize);
+            if ((Input.mousePosition.x <= 0
+                &&Camera.main.WorldToScreenPoint(leftdown.transform.position).x< remainpixel)
+                || Camera.main.WorldToScreenPoint(rightup.transform.position).x < Screen.width- remainpixel)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed * Camera.main.orthographicSize);
+            }
+            if ((Input.mousePosition.x >= Screen.width
+                && Camera.main.WorldToScreenPoint(rightup.transform.position).x > Screen.width- remainpixel)
+                || Camera.main.WorldToScreenPoint(leftdown.transform.position).x > remainpixel)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed * Camera.main.orthographicSize);
+            }
+            if ((Input.mousePosition.y >= Screen.height
+                && Camera.main.WorldToScreenPoint(rightup.transform.position).y > Screen.height - remainpixel)
+                || Camera.main.WorldToScreenPoint(leftdown.transform.position).y > remainpixel)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * speed * Camera.main.orthographicSize);
+            }
+            if ((Input.mousePosition.y <= 0
+                && Camera.main.WorldToScreenPoint(leftdown.transform.position).y < remainpixel)
+                || Camera.main.WorldToScreenPoint(rightup.transform.position).y < Screen.height - remainpixel)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * speed * Camera.main.orthographicSize);
+            }
         }
-        if ((Input.mousePosition.y <= 0
-            && Camera.main.WorldToScreenPoint(leftdown.transform.position).y < remainpixel)
-            || Camera.main.WorldToScreenPoint(rightup.transform.position).y < Screen.height - remainpixel)
-        {
-            transform.Translate(Vector3.down * Time.deltaTime * speed * Camera.main.orthographicSize);
-        }
+
     }
 }
