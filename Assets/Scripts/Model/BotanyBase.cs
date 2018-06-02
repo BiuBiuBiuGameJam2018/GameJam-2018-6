255286b90d0e8a34b0677288bfcc068717fb6dd3
WangDaxia57 <wang987662210@163.com>
2018-06-02 09:52:48 +0800

    ddd

--- Assets/Scripts/Model/BotanyBase.cs
+++ Assets/Scripts/Model/BotanyBase.cs
@@ -15,42 +15,45 @@ public enum EnumBotanyState
 
 public class Objbase
 {
-   
+
 }
 public class BotanyBase : Objbase
 {
-    public BotanyBase father=null;
-    public float scale = 1;
-    public float reducescale=0.95f;//每一级的缩放比例
+    public BotanyBase father = null;
+
 
     public EnumBotanyType BotanyType;
     public EnumBotanyState BotanyState;
     public TrailRenderer MainTrail;
     public GameObject SphereNode;
     public GameObject target;
-    
+
     //data
-   protected  int Serialnumber;
+    protected int Serialnumber;
     public int BranchConsumption;//分支消耗 需要配置
-
-
-    public BotanyBase(Transform tran) {
+    public float scale = 1;
+    public float reducescale = 0.95f;//每一级的缩放比例
+    public BotanyBase(Transform tran)
+    {
         Create(tran);
     }
     /// <summary>
     /// 创建时调用
     /// </summary>
     /// <param name="pos"></param>
-     void Create(Transform tran) {
-        Object node = Resources.Load("Botany/node") ;
+    void Create(Transform tran)
+    {
+        Object node = Resources.Load("Botany/node");
         SphereNode = UnityEngine.Object.Instantiate(node, tran.position, tran.rotation) as GameObject;
-        MainTrail = new GameObject().AddComponent<TrailRenderer>();
+        MainTrail = SphereNode.transform.Find("Trail").gameObject.GetComponent<TrailRenderer>();
         MainTrail.name = MainManger.Instance.serialnumber.ToString("00");
         Serialnumber = MainManger.Instance.serialnumber;
         if (MainManger.Instance.Bg != null)
             MainTrail.transform.position = MainManger.Instance.Bg.transform.position - new Vector3(0, 0, 5);
         MainTrail.transform.SetParent(MainManger.Instance.TrailRoot.transform);
         MainTrail.time = 360000000000f;
+        MainTrail.startWidth *= scale;
+        MainTrail.endWidth *= scale;
         MainTrail.transform.position = new Vector3(tran.position.x, tran.position.y, MainTrail.transform.position.z);
 
         target = SphereNode.transform.Find("target").gameObject;
@@ -59,9 +62,8 @@ public class BotanyBase : Objbase
             scale = father.scale * reducescale;
         }
         SphereNode.transform.localScale *= scale;//缩放
-        SphereNode.transform.Find("trail").gameObject.GetComponent<LineRenderer>().startWidth *= scale;
-        SphereNode.transform.Find("trail").gameObject.GetComponent<LineRenderer>().endWidth *= scale;
-        
+
+
     }
 
 }
