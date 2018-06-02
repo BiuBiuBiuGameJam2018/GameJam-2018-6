c057ab19cbe33ad9fb6c9869c65b21cea1e36da1
WangDaxia57 <wang987662210@163.com>
2018-06-02 08:19:24 +0800

    sss

--- Assets/Scripts/Model/BotanyBase.cs
+++ Assets/Scripts/Model/BotanyBase.cs
@@ -24,6 +24,7 @@ public class BotanyBase : Objbase
     public GameObject SphereNood;
 
     //data
+   protected  int Serialnumber;
     public int BranchConsumption;//分支消耗 需要配置
 
 
@@ -37,12 +38,12 @@ public class BotanyBase : Objbase
      void Create(Vector2 pos) {
         MainTrail = new GameObject().AddComponent<TrailRenderer>();
         MainTrail.name = MainManger.Instance.serialnumber.ToString("00");
+        Serialnumber = MainManger.Instance.serialnumber;
         if (MainManger.Instance.Bg != null)
             MainTrail.transform.position = MainManger.Instance.Bg.transform.position - new Vector3(0, 0, 5);
         MainTrail.transform.SetParent(MainManger.Instance.TrailRoot.transform);
         MainTrail.time = 360000000000f;
         MainTrail.transform.position = new Vector3(pos.x, pos.y, MainTrail.transform.position.z);
-        MainManger.Instance.serialnumber++;
 
 
     }
